9c0036f75dde22b53fbe2a28b4231b74c58de74f
jopego12 <jopego12@gmail.com>
2026-06-24 15:26:48 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,24 @@
 const CACHE_NAME = "techtrack-v20260624";
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
-      ]);
-    })
-  );
+self.addEventListener("install", event => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  const url = new URL(e.request.url);
-
-  // No tocar APIs externas
-  if (url.origin !== location.origin) return;
-
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
